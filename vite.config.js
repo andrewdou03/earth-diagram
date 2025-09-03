1f93608f35d1843d18e9fe3acc292fc7f8d5c603
Andrew Dou <andrew.dou2003@gmail.com>
2025-09-02 23:51:10 -0400

    even more vercel changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import glsl from "vite-plugin-glsl";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,12 @@ export default defineConfig({
       include: "**/*.glsl", // or .vert/.frag
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        embed: resolve(__dirname, 'embed.html'),
+      },
+    },
+  }
 });
